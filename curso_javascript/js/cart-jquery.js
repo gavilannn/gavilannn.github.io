f6fe8b94d5cad908eea3da566f7563bba207ceb6
Tienda Viaggio Te <desarrollo@balnes.eu>
2021-12-01 02:05:45 -0300

    --

--- curso_javascript/js/cart-jquery.js
+++ curso_javascript/js/cart-jquery.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
         localStorage.removeItem('Carrito');
         localStorage.removeItem('listaActualizada'); 
         $("#cartItems > div").remove();
+        $(".total").addClass("d-none");
         $("#cartItems").append(`<div class="w-100 h-100 d-flex justify-content-center align-items-center">
         <h3 class="text-secondary my-auto text-center"><i class="fas fa-shopping-cart fa-2x"></i><br>No hay productos en el carrito</h3></div>`);
 
@@ -26,23 +27,33 @@ $(document).ready(function () {
 function createCart() {
     let cartItems = JSON.parse(localStorage.getItem('Carrito'));
 
+    let totalCart = 0;
+
+
     if (cartItems) {
 
-        $("#cartItems > div").remove();
+        $("#cartItems > div").remove();        
 
         cartItems.forEach(p => {
             $("#cartItems").append(`<div class="d-block rounded border text-left px-4 py-2">
-            <span>${(p.productInCart)}x${p.productType}&nbsp;${p.productName}-${p.productVariant}&nbsp;|&nbsp;${p.productPresentation}&nbsp;|&nbsp;${p.productPresentationPack}&nbsp;x${p.productPresentationQty}</span>
+            <span>${(p.productInCart)}x&nbsp;${p.productPresentationPack}&nbsp;x${p.productPresentationQty}&nbsp;|&nbsp;${p.productType}&nbsp;${p.productName}-${p.productVariant}&nbsp;|&nbsp;${p.productPresentation}&nbsp;</span>
             <span class="d-block fw-bold">Subtotal: $${(p.productPrice)*(p.productPresentationQty)*(p.productInCart)}</span>
-            </div>`);        
+            </div>`); 
+            totalCart = totalCart + ((p.productPrice)*(p.productPresentationQty)*(p.productInCart));
         });
 
+        $("#totalCart > span").text(totalCart);
+
     } else {
+
+        $(".total").addClass("d-none");
 
         $("#cartItems").append(`<div class="w-100 h-100 d-flex justify-content-center align-items-center">
         <h3 class="text-secondary my-auto text-center"><i class="fas fa-shopping-cart fa-2x"></i><br>No hay productos en el carrito</h3></div>`);        
 
     }   
+
+
     
 }
 
