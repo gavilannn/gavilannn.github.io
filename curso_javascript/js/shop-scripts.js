af88b81d006d1d03da62bced116c91e60c773ae5
Tienda Viaggio Te <desarrollo@balnes.eu>
2021-12-09 10:12:10 -0300

    --

--- curso_javascript/js/shop-scripts.js
+++ curso_javascript/js/shop-scripts.js
@@ -12,3 +12,49 @@ if (userBornSave == null) {
     bodySelector.classList.remove('d-none');
 
 }
+
+$(function () {
+
+  $(".animate-banner.yes > h2").hide().fadeIn(1000);
+
+  $(".animate-banner").eq(1).addClass("not");
+  $(".animate-banner").eq(2).addClass("not");
+
+    let pos = 0; 
+    let lenghtBanner = parseInt($(".animate-banner").length) - 1;
+
+  $("#next-banner").on("click", function (e) {
+
+    e.preventDefault();
+    
+    if ((pos >= 0) && (pos < lenghtBanner)) {        
+        pos = pos + 1; 
+        
+        $(".animate-banner.yes").addClass("not").removeClass("yes");    
+        $(".animate-banner.not").eq(pos).addClass("yes").removeClass("not");
+        $(".animate-banner.yes > h2").hide().fadeIn(1000);
+
+
+    }
+
+  });
+
+  $("#prev-banner").on("click", function (e) {
+
+    e.preventDefault();
+    
+    if ((pos > 0)) {
+        pos = pos - 1; 
+
+        $(".animate-banner.yes").addClass("not").removeClass("yes");    
+        $(".animate-banner.not").eq(pos).addClass("yes").removeClass("not");
+        $(".animate-banner.yes > h2").hide().fadeIn(1000);
+
+    } else {
+     pos = 0;
+    }
+      
+  });
+
+
+});
